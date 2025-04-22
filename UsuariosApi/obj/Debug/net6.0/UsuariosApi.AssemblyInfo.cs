c248952dc5a07702fa025b6d1f28ba3deecf1b92
lucianorb19 <52081214+lucianorb19@users.noreply.github.com>
2025-04-22 20:19:17 -0300

    Automatic updates

--- UsuariosApi/obj/Debug/net6.0/UsuariosApi.AssemblyInfo.cs
+++ UsuariosApi/obj/Debug/net6.0/UsuariosApi.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("UsuariosApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dc367668e071c0f284f8f0e91da439bc4a92e4eb")]
 [assembly: System.Reflection.AssemblyProductAttribute("UsuariosApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("UsuariosApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
